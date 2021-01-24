0beeb82e6c0c9773f1f5ac57f918526b912f407e
Yannick Rehberger <yr@ityreh.de>
2021-01-24 13:25:43 +0100

    Add bootstrap.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,38 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+class Navbar extends React.Component {
+  render() {
+    return (
+      <nav class="navbar navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            <img src="logo192.png" alt="logo" width="30" height="30" class="d-inline-block align-top" />
+              pomodoro
+          </a>
+        </div>
+      </nav>
+    )
+  }
+}
+
+class Footer extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <footer class="footer mt-auto py-3 bg-light">
+        <div class="container">
+          <span class="me-auto text-muted">Version: {this.props.version}</span>
+        </div>
+      </footer>
+    )
+  }
+}
 
 class Clock extends React.Component {
   constructor(props) {
@@ -34,12 +66,21 @@ class Clock extends React.Component {
   }
 }
 
-function App() {
-  return (
-    <div>
-      <Clock />
-    </div>
-  )
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { version: "0.0.1" }
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <Clock />
+        <Footer version={this.state.version} />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(
